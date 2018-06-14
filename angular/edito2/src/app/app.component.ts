de772151becddaa9b06766a3f19a00cbd9f012bf
Carlos Alberto <carlos.rodrigues@capes.gov.br>
2018-06-14 16:41:25 -0300

    melhorado o visual da interface

--- angular/edito2/src/app/app.component.ts
+++ angular/edito2/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, NgZone, OnInit, ViewContainerRef, ViewChild, ElementRef, Cha
 
 import { TreeViewComponent } from "./tree-view/tree-view.component";
 
-import {Comp, ComponentType, UIComponent, FlatNode, PropertyType } from "./model";
+import { Comp, Type, ComponentFactory, ComponentType, UIComponent, FlatNode, PropertyType } from "./model";
 
 @Component({
   selector: 'app-root',
@@ -14,18 +14,20 @@ export class AppComponent implements OnInit {
   components: Array<UIComponent> = [];
 
   devices: Array<Device> = [];
-  _deviceSelected : Device;
+  _deviceSelected: Device;
   width: string;
   height: string;
-  zoom:number = 100;
+  zoom: number = 100;
 
   @ViewChild('editorContainer') editor: ElementRef;
   editorWidth: number;
 
-  rotate(){
+  rotate() {
     let tmp = this.width;
     this.width = this.height;
     this.height = tmp;
+
+    this.adjustZoom();
   }
 
   constructor(private renderer: Renderer2) {
@@ -34,26 +36,44 @@ export class AppComponent implements OnInit {
     this.devices.push(new Device("Laptop", '1280px', '800px'));
     this.devices.push(new Device("Desktop", '1920px', '1080px'));
 
-    
+
 
   }
 
   ngOnInit(): void {
 
+
+
+    class DummyComponentFactory implements ComponentFactory {
+
+      friendlyName: string;
+      elementType: Type;
+      properties: PropertyType[] = [];
+
+      constructor() { }
+
+      render(): string {
+        throw new Error('Method not implemented.');
+      }
+      isComponent(data: any): boolean {
+        throw new Error('Method not implemented.');
+      }
+
+    }
     console.log(Comp.components)
-    let viewWindow : any = window;
-    this.editorWidth = parseInt(viewWindow.document.defaultView.getComputedStyle(this.editor.nativeElement).width.replace('px',''))-10;
+    let viewWindow: any = window;
+    this.editorWidth = parseInt(viewWindow.document.defaultView.getComputedStyle(this.editor.nativeElement).width.replace('px', '')) - 10;
 
     this.deviceSelected = this.devices[2];
 
     let form = new UIComponent();
-    form.type = new ComponentType();
-    form.type.name = "form";
+    form.type = new DummyComponentFactory();
+    form.type.friendlyName = "form";
     this.components.push(form)
 
     let ff = new UIComponent();
-    ff.type = new ComponentType();
-    ff.type.name = "form-field";
+    ff.type = new DummyComponentFactory();
+    ff.type.friendlyName = "form-field";
     form.children.push(ff);
     let pt = new PropertyType();
     pt.key = "placeHolder"
@@ -63,44 +83,48 @@ export class AppComponent implements OnInit {
     ff.type.properties.push(pt)
 
     let ff1 = new UIComponent();
-    ff1.type = new ComponentType();
-    ff1.type.name = "form-field2";
+    ff1.type = new DummyComponentFactory();
+    ff1.type.friendlyName = "form-field2";
     form.children.push(ff1);
 
     ff = new UIComponent();
-    ff.type = new ComponentType();
-    ff.type.name = "input";
+    ff.type = new DummyComponentFactory();
+    ff.type.friendlyName = "input";
     ff1.children.push(ff);
 
     ff = new UIComponent();
-    ff.type = new ComponentType();
-    ff.type.name = "error-message";
+    ff.type = new DummyComponentFactory();
+    ff.type.friendlyName = "error-message";
     ff1.children.push(ff);
 
   }
 
-set deviceSelected(device:Device){
-  this.width = device.width;
-  this.height = device.height;
-  this._deviceSelected = device;
-  let viewWidth: number = parseInt(device.width.replace('px',''));
-  let calculedZoom: number = this.editorWidth/viewWidth;
-  if (calculedZoom>1){
-    this.zoom = 100;
-  } else {
-    this.zoom = parseInt((calculedZoom*100).toString());
+  set deviceSelected(device: Device) {
+    this.width = device.width;
+    this.height = device.height;
+    this._deviceSelected = device;
+    this.adjustZoom();
+
   }
-  
-}
 
+  private adjustZoom() {
+    let viewWidth: number = parseInt(this.width.replace('px', ''));
+    let calculedZoom: number = this.editorWidth / viewWidth;
+    if (calculedZoom > 1) {
+      this.zoom = 100;
+    } else {
+      this.zoom = parseInt((calculedZoom * 100).toString());
+    }
 
-get deviceSelected(){
-  return this._deviceSelected;
-}
+  }
+
+  get deviceSelected() {
+    return this._deviceSelected;
+  }
 
 }
 
 
 class Device {
-  constructor(public name: string,public width: string,public height: string) { }
+  constructor(public name: string, public width: string, public height: string) { }
 }
